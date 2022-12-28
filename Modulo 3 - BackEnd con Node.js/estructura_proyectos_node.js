5bfcac0984116cc5e5d7e4ce0e9a0fc77384e183
Consuelo de Maria <ceinnova10339@innovaccion.mx>
2022-12-28 15:40:18 -0600

    Mis practicas de BackEnd

---
+++ Modulo 3 - BackEnd con Node.js/estructura_proyectos_node.js
@@ -0,0 +1,8 @@
+node_modules
+package-lock.json
+/modules/retrievedate.js
+
+// Este módulo lo creamos y solo devuelve la fecha actual DEL SERVER
+exports.RetrieveDate = function() {
+    return Date();
+}
